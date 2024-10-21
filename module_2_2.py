42262b1153ff44570318d1d945cbe3703e567768
greenman983 <greenman983983@gmail.com>
2024-10-22 01:17:22 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -1,9 +1,9 @@
 first = input('Веедите первое число: ')
 second = input('Введите второе число: ')
 third = input('Введите третье число: ')
-if first == second == third:
+if first == second == third: # если все равны
     print(3)
-elif first == second or first == third or second == third:
+elif first == second or first == third or second == third: # если равны два из трех
     print(2)
-elif first != second != third:
+elif first != second != third: # если все не равны
     print(0)
