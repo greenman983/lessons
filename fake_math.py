8729a9081c96f0809425c7fe5be1ddcc4d5c4074
greenman983 <greenman983983@gmail.com>
2024-11-24 22:39:53 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,7 @@
+def divide(first, second):
+    if second == 0:
+        return 'ошибка'
+    else:
+        answer = first / second
+        return answer
+
---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as div_1
+from true_math import divide as div_2
+
+res_1 = div_1(12, 0)
+res_2 = div_1(44, 4)
+res_3 = div_2(45, 0)
+res_4 = div_2(66, 3)
+print(res_1)
+print(res_2)
+print(res_3)
+print(res_4)
